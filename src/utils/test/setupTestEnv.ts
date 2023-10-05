b147481151ece759d3aefe57585fd15880029ecd
Maciej Stosio <maciekstosio@users.noreply.github.com>
2023-10-05 20:56:33 +0200

    setup tests

---
+++ src/utils/test/setupTestEnv.ts
@@ -0,0 +1,10 @@
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock'
+
+jest.mock('react-native-safe-area-context', () => mockSafeAreaContext)
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
+
+// https://stackoverflow.com/questions/75934507/after-upgrading-to-expo48-meeting-this-error-typeerror-cannot-read-properties
+jest.mock("expo-font")
+jest.mock("expo-asset")
