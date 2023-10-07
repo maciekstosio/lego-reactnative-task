0056869ea6186f383adbdb4f51ae56a588fdee68
Maciej Stosio <maciekstosio@users.noreply.github.com>
2023-10-07 14:35:46 +0200

    Change folder with theme

--- src/components/LoaderScreen.tsx
+++ src/components/LoaderScreen.tsx
@@ -1,5 +1,5 @@
 import {LoaderScreen as RNUILoaderScreen} from 'react-native-ui-lib'
-import {legoRed} from '@/theme'
+import {legoRed} from '@/utils/theme'
 
 const LoaderScreen = () => <RNUILoaderScreen message="Loading..." loaderColor={legoRed} />
 
