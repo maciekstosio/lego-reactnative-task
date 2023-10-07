b74d2613b4737e3c1aded88f65c1c52a33dbb30f
Maciej Stosio <maciekstosio@users.noreply.github.com>
2023-10-07 14:33:54 +0200

    Fix tests after introducing env file

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
+const MOCKED_URL = 'http://mock.com/api/v3/lego'
+const MOCKED_KEY = 'superSecretKey'
+
 export default {
-    apiKey: process.env.EXPO_PUBLIC_API_KEY,
-    apiBaseUrl: process.env.EXPO_PUBLIC_API_URL
+    apiKey: process.env.EXPO_PUBLIC_API_KEY ?? MOCKED_KEY,
+    apiBaseUrl: process.env.EXPO_PUBLIC_API_URL ?? MOCKED_URL
 }
