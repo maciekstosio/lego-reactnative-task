9ef2c14f7e99fed16bf93acab9a03bf0926761a5
Maciej Stosio <maciekstosio@users.noreply.github.com>
2023-10-07 22:56:49 +0200

    Export WebViewModal

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,4 @@
 export {default as PersonalDetailsScreen} from './PersonalDetailsScreen'
 export {default as SummaryScreen} from './SummaryScreen'
 export {default as RandomScreen} from './RandomScreen'
+export {default as WebViewModal} from './WebViewModal'
